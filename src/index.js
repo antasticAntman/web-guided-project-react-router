1c88bf5fec02d3970f07529a3faab191bb57d569
Anthony Climer <anthonyclimer0925@gmail.com>
2023-01-11 17:53:14 -0500

    Did it!

--- src/index.js
+++ src/index.js
@@ -8,9 +8,18 @@ import App from './components/App'
 import './styles.less'
 
 // 👉 STEP 1 - Import React Router's Router
-
+import { BrowserRouter as Router} from 'react-router-dom' ;
 render(
   // Wrap the <App /> in a provider
+  <Router>
   <App />
+  </Router>
   , document.querySelector('#root')
 )
+
+
+/*
+* Location -> All data about where you are
+* History -> All data about where you were
+* Match -> All data about the current location w/re to query param etc etc
+*/
